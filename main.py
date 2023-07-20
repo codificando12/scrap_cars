65f65e06d169a0a122631abfb60bfb40e88498f0
Emilio <planeta.chompi18@gmail.com>
2023-07-20 14:32:14 +0800

    trying new ways because it get stuck

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def run():
     driver = webdriver.Chrome(executable_path = r'C:/chrome-driver/linux/chromedriver.exe')
     
     driver.get('https://www.kijijiautos.ca/cars/#od=down&sb=rel&st=FSBO')
-    cars = driver.find_elements(By.TAG_NAME, 'article')
     first_art = 1
     list_div = 1
     
@@ -28,50 +27,57 @@ def run():
         elements_div_path = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, div_path))) 
         articles = elements_div_path.find_elements(By.TAG_NAME, 'article')
         print(len(articles))
+        
         for article in range(len(articles) + 1):
-            print(div_path)
-            
-                # click_article = WebDriverWait(driver, 10).until(
-                #     EC.element_to_be_clickable(
-                #     (By.XPATH, f'//*[@id="root"]/div[3]/div/section[5]/div/div/div[2]/div/div[2]/div/div{[article + 1]}/article')
-                #     )
-                # )
-            click_article = driver.find_element(By.XPATH, f'{div_path}/div{[article + 1]}') 
-                #     )')
-            div_class = click_article.get_attribute('class')
-            print(div_class)
-            if div_class == 'h2GjMu':
-                continue
-            else:
-                time.sleep(2)
-                click_article.click()
-                title = driver.find_element(By.TAG_NAME, 'h1')
-                print(title.text)
-                current_url = driver.current_url
-                print(current_url)
-                time.sleep(1)
-                try:
-                    WebDriverWait(driver, 2).until(
-                        EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[3]/div[3]/section/div/nav/div[2]/button[1]/span/span'))
-                    ).click()
-                    time.sleep(0.5)
-                    number = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[3]/section/div/nav/div[2]/button[1]/span/span')
-                    print(number.text)
-                    driver.back()
+                print(div_path)
+                
+                    # click_article = WebDriverWait(driver, 10).until(
+                    #     EC.element_to_be_clickable(
+                    #     (By.XPATH, f'//*[@id="root"]/div[3]/div/section[5]/div/div/div[2]/div/div[2]/div/div{[article + 1]}/article')
+                    #     )
+                    # )
+                click_article = driver.find_element(By.XPATH, f'{div_path}/div{[first_art]}') 
+                    #     )')
+                div_class = click_article.get_attribute('class')
+                print(div_class)
+                if div_class == 'h2GjMu':
+                    continue
+                else:
+                    time.sleep(2)
+                    actions = ActionChains(driver)
+                    actions.move_to_element(click_article).click().perform()
+                    title = driver.find_element(By.TAG_NAME, 'h1')
+                    print(title.text)
+                    current_url = driver.current_url
+                    print(current_url)
                     time.sleep(1)
-                    # driver.execute_script("window.scrollBy(0, 50);")
-                except:
-                    print('no phone')
-                    driver.back()
-                    time.sleep(1)
-                    print(article)
-                    time.sleep(1)
-                    # driver.execute_script("window.scrollBy(0, 50);")
-                    
+                    first_art += 1
+                    try:
+                        WebDriverWait(driver, 2).until(
+                            EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[3]/div[3]/section/div/nav/div[2]/button[1]/span/span'))
+                        ).click()
+                        time.sleep(0.5)
+                        number = driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[3]/section/div/nav/div[2]/button[1]/span/span')
+                        print(number.text)
+                        driver.back()
+                        time.sleep(3)
+                        driver.execute_script("window.scrollBy(0, 300);")
+                    except:
+                        print('no phone')
+                        driver.back()
+                        time.sleep(1)
+                        print(article)
+                        driver.execute_script("window.scrollBy(0, 300);")
+                        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'div')))
+        # except:
+        #     time.sleep(4)
+        #     continue
+
         first_art = 1
         list_div += 1
         div_path = f'//*[@id="root"]/div[3]/div/section[5]/div/div/div[2]/div/div[2]/div{[list_div]}'
         print(count_articles)
+        driver.execute_script("window.scrollBy(0, 100);")
         
             
     time.sleep(30)
